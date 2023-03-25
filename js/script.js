22a48fd6197cc78cdb3bb9928db9af7b46cf28ca
amber <amberdeep52@gmail.com>
2023-03-25 21:27:53 +0530

    assignment

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showfunction(){
+    navigationbtn = document.querySelector(".journey");
+    navigationbtn.classList.toggle("active");
+}
+function noticeFunction(){
+    notice = document.querySelector(".notice");
+    notice.classList.toggle("active");
+}
